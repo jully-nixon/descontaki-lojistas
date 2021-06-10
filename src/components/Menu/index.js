02c16d5d7873508a5bb840f383b68989f920204c
DIR\jully.nixon <74429270+jully-nixon@users.noreply.github.com>
2021-06-10 15:29:39 -0300

    Correção título da home.

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -5,7 +5,7 @@ import './style.css';
 const MenuSide = () => {
     return (
         <Menu>
-            <a id="home" className="menu-item" href="/home">Home</a>
+            <a id="home" className="menu-item" href="/home">Início</a>
             <a id="profile" className="menu-item" href="/profile">Perfil</a>
             <a id="check-coupon" className="menu-item" href="/check-coupons">Verificar Cupom</a>
             <a id="reports" className="menu-item" href="/reports">Relatórios</a>
